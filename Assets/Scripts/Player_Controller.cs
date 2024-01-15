315af15803561a7427a937144b3de87af3aeb41c
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-01-15 17:29:20 +0530

    Fixed Screen Bonds

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,3 +1,5 @@
+
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
@@ -10,9 +12,18 @@ public class Player_Controller : MonoBehaviour
     private Camera mainCam;
     private Vector3 offSet;
 
+    private float maxLeft;
+    private float maxRight;
+
+    private float maxUp;
+    private float maxDown;
+
     void Start()
     {
         mainCam = Camera.main;
+
+        
+
     }
 
 
@@ -42,6 +53,18 @@ public class Player_Controller : MonoBehaviour
                 transform.position = new Vector3(touchPos.x - offSet.x, touchPos.y - offSet.y,0);
             }
 
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x,maxLeft,maxRight),Mathf.Clamp(transform.position.y, maxDown,maxUp),0);
+
         }
+    }
+
+    private IEnumerator SetBoundaries(){
+        yield return new WaitForSeconds(0.2f);
+
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f,0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f,0)).x;
+
+        maxDown = mainCam.ViewportToWorldPoint(new Vector2(0, 0.07f)).y;
+        maxUp = mainCam.ViewportToWorldPoint(new Vector2(0,0.9f)).y;
     }
 }
