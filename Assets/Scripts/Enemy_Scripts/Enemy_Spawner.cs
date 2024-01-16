8a3a4c98c884457467fa11fe05fd2a47c8a19090
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-01-16 18:06:33 +0530

    Enemy_Spawner_EnemyAnimation

---
+++ Assets/Scripts/Enemy_Scripts/Enemy_Spawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour
+{
+    private Camera mainCam;
+    private float maxLeft;
+    private float maxRight;
+    private float yPos;
+
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject[] enemys_Prefabs;
+    private float enemyTimer;
+    [Space(15)]
+    [SerializeField]private float enemySpawnTime;
+
+
+    void Start()
+    {
+        mainCam = Camera.main;
+
+        StartCoroutine(SetBoundaries());
+    }
+
+    private void Update() {
+        Spawn_Enemy();
+    }
+
+    private void Spawn_Enemy(){
+        enemyTimer += Time.deltaTime;
+        if(enemyTimer >= enemySpawnTime){
+            int randomPick = Random.Range(0,enemys_Prefabs.Length);
+            Instantiate(enemys_Prefabs[randomPick], new Vector3(Random.Range(maxLeft, maxRight),yPos,0), Quaternion.identity);
+            enemyTimer = 0;
+        }
+    }
+    
+    private IEnumerator SetBoundaries(){
+        yield return new WaitForSeconds(0.2f);
+
+        maxLeft = mainCam.ViewportToWorldPoint(new Vector2(0.15f,0)).x;
+        maxRight = mainCam.ViewportToWorldPoint(new Vector2(0.85f,0)).x;
+
+        yPos = mainCam.ViewportToWorldPoint(new Vector2(0,1.1f)).y;
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy_Scripts/Green_Enemy.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Green_Enemy : Enemy
+{
+    [SerializeField] private float speed;
+  
+    void Start()
+    {
+        rb.velocity = Vector2.down * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.CompareTag("Player")){
+            collider.GetComponent<Player_State>().Player_TakeDamage(damage);
+            Instantiate(explosion_Prefab, transform.position,transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    public override void Hurt_Sequence()
+    {
+        if(anim.GetCurrentAnimatorStateInfo(0).IsTag("Green_Dmg"))
+            return;
+        anim.SetTrigger("Damage");
+    }
+
+    public override void Death_Sequence()
+    {
+        Instantiate(explosion_Prefab, transform.position,transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
