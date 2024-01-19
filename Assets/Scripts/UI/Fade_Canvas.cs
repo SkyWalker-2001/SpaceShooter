2c330e861ef60ac148b30826f98486b66d9e26c2
SkyWalker-2001 <gagansingh2001@outlook.com>
2024-01-19 00:36:40 -0800

    Loading_screen and Loadng_Bar

--- Assets/Scripts/UI/Fade_Canvas.cs
+++ Assets/Scripts/UI/Fade_Canvas.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Fade_canvas : MonoBehaviour
 {
     public static Fade_canvas fader;
 
     [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private GameObject loading_Screen;
+    [SerializeField] private Image loading_Bar;
     [SerializeField] private float changeValue;
     [SerializeField] private float wait_Time;
     [SerializeField] private bool fadeStarted = false;
@@ -41,9 +44,13 @@ public class Fade_canvas : MonoBehaviour
 
     private IEnumerator FadeIn()
     {
+        loading_Screen.SetActive(false);
+
         fadeStarted = false;
         while(_canvasGroup.alpha > 0)
         {
+            if(fadeStarted)
+                yield break;
             _canvasGroup.alpha -= changeValue;
             yield return new WaitForSeconds(wait_Time); 
         }
@@ -51,6 +58,10 @@ public class Fade_canvas : MonoBehaviour
 
     private IEnumerator FadeOut_String(string level_Name)
     {
+        // Until fade in disappear player controles is on hold 
+        //if (_canvasGroup.alpha != 0)
+        //    yield break;
+
         if(fadeStarted)
             yield break;
         fadeStarted = true;
@@ -59,12 +70,38 @@ public class Fade_canvas : MonoBehaviour
             _canvasGroup.alpha += changeValue;
             yield return new WaitForSeconds(wait_Time);
         }
-        SceneManager.LoadScene(level_Name);
-        yield return new WaitForSeconds(.1f);
+        //SceneManager.LoadScene(level_Name);
+
+        // --------------------------------------------------------------IMP------------------------------------------ //
+
+        // 0 --- 0.9 --- 1
+
+        // https://www.udemy.com/course/unitymobilecourse/learn/lecture/37102592#content
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(level_Name);
+        ao.allowSceneActivation = false;
+        loading_Screen.SetActive(true);
+        loading_Bar.fillAmount = 0;
+
+        while (ao.isDone == false)
+        {
+            loading_Bar.fillAmount = ao.progress / 0.9f;
+
+            if(ao.progress == 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+
+
+        // ---------------------------------------------------------------------------------------------------------- //
+
+        // yield return new WaitForSeconds(.1f);
         StartCoroutine(FadeIn());
     }  
     
-    private IEnumerator FadeOut_Int(int level_Number)
+    private IEnumerator FadeOut_Int(int level_Index)
     {
         if(fadeStarted)
             yield break;
@@ -74,8 +111,25 @@ public class Fade_canvas : MonoBehaviour
             _canvasGroup.alpha += changeValue;
             yield return new WaitForSeconds(wait_Time);
         }
-        SceneManager.LoadScene(level_Number);
-        yield return new WaitForSeconds(.1f);
+        //SceneManager.LoadScene(level_Number);
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(level_Index);
+        ao.allowSceneActivation = false;
+        loading_Screen.SetActive(true);
+        loading_Bar.fillAmount = 0;
+
+        while (ao.isDone == false)
+        {
+            loading_Bar.fillAmount = ao.progress / 0.9f;
+
+            if (ao.progress == 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+
+        //yield return new WaitForSeconds(.1f);
         StartCoroutine(FadeIn());
     }
 }
